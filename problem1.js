cb5e91c0b7a3e9fdfd32b7ab39eaed0a0b5dda7d
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-15 10:41:12 +0530

    import fs and path module and create fileSystemAndRemoving function which create first directory and then random json file and lastly removed it.

--- problem1.js
+++ problem1.js
@@ -3,3 +3,54 @@
 // 1. Create a directory of random JSON files
 // 2. Delete those files simultaneously
 const fs = require('fs');
+const path = require('path');
+
+const fileSystemAndRemoving = (data, number) => {
+  const dirname = './RandomJsonFile';
+  if (Object.keys(data).length === 0 || !number) {
+    throw new Error('data is Empty or not a number');
+  }
+
+  try {
+    fs.mkdir(path.join(__dirname, dirname), (err) => {
+      if (err) {
+        throw new Error('unable to create directory');
+      }
+    });
+
+    for (let index = 1; index <= number; index++) {
+      fs.writeFile(
+        `./RandomJsonFile/${index}.json`,
+        JSON.stringify(data),
+        'utf-8',
+        (error) => {
+          if (error) {
+            throw new Error('File path is not correct.');
+          }
+        }
+      );
+    }
+
+    setTimeout(() => {
+      for (let index = 1; index <= number; index++) {
+        let filename = path.join(__dirname, dirname, `${index}.json`);
+
+        fs.unlink(filename, (error) => {
+          if (error) {
+            throw new Error('unable to find file location');
+          }
+        });
+      }
+
+      fs.rmdir(path.join(__dirname, dirname), (error) => {
+        if (error) {
+          throw new Error('File Does Not Exists ...');
+        }
+      });
+    }, 5000);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = fileSystemAndRemoving;
