4d1b457648e273006430326c3274061f9ef09e73
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 10:48:03 +0530

    declare fileSystemAndRemoving function in async by async keyword and resolve promise by await keyword.

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -5,46 +5,32 @@ const {
   removeDirectory,
 } = require('../problem1');
 
-const fileSystemAndRemoving = () => {
-  let number = Number((Math.random() * 10).toFixed(0));
-  let jsonData = JSON.stringify({ name: 'dharmendra', age: 24 });
-  const dirname = './RandomJsonFile';
+const fileSystemAndRemoving = async () => {
+  try {
+    let number = Number((Math.random() * 10).toFixed(0));
+    let jsonData = JSON.stringify({ name: 'dharmendra', age: 24 });
+    const dirname = './RandomJsonFile';
 
-  createDirectory(dirname)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.log(error.message));
+    const msgForDir = await createDirectory(dirname);
+    console.log(msgForDir);
 
-  for (let index = 1; index <= number; index++) {
-    let filename = `${dirname}/${index}.json`;
-    writeFile(filename, jsonData)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+    for (let index = 1; index <= number; index++) {
+      let filename = `${dirname}/${index}.json`;
+      let msgForWrite = await writeFile(filename, jsonData);
+      console.log(msgForWrite);
+    }
+
+    for (let index = 1; index <= number; index++) {
+      let filename = `${dirname}/${index}.json`;
+      let msgForDelete = await DeleteFile(filename);
+      console.log(msgForDelete);
+    }
+
+    let msgForRemoveDir = await removeDirectory(dirname);
+    console.log(msgForRemoveDir);
+  } catch (error) {
+    console.log(error.message);
   }
-
-  for (let index = 1; index <= number; index++) {
-    let filename = `${dirname}/${index}.json`;
-    DeleteFile(filename)
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }
-
-  removeDirectory(dirname)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
 };
 
 fileSystemAndRemoving();
