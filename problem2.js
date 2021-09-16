e79549cf31d3fe0facc2867ee1d83dbbdf615182
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 10:33:46 +0530

    create writeFile, readFile, DeleteFile Promise and export it for reusable.

--- problem2.js
+++ problem2.js
@@ -1,102 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const fileSystemOperation = () => {
-  try {
-    fs.readFile(path.join('./lipsum.txt'), 'utf-8', (error, data) => {
+const writeFile = (filename, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, 'utf-8', (error) => {
       if (error) {
-        throw new Error('lipsum.txt file does not exists..');
+        reject('File path is not correct.');
       } else {
-        let upperCaseData = data.toUpperCase();
-        console.log('lipsum.txt File Readed..');
-
-        fs.writeFile('fileNames.txt', upperCaseData, 'utf-8', (error) => {
-          if (error) {
-            throw new Error('File Does Not Exists');
-          } else {
-            console.log('fileNames.txt File is created..');
-
-            let filePath = path.join(__dirname, 'fileNames.txt');
-
-            fs.readFile(filePath, 'utf-8', (error, data) => {
-              if (error) {
-                throw new Error(`Error is ${error}`);
-              } else {
-                let lowerCaseData = JSON.stringify(
-                  data.toLowerCase().split('.').join('')
-                );
-
-                fs.writeFile(
-                  'fileNames.txt',
-                  lowerCaseData,
-                  'utf-8',
-                  (error) => {
-                    if (error) {
-                      throw new Error('fileNames.txt file does not exists');
-                    } else {
-                      console.log('fileNames.txt File is created..');
-
-                      fs.readFile('fileNames.txt', 'utf-8', (error, data) => {
-                        if (error) {
-                          throw new Error('fileNames.txt file does not exists');
-                        } else {
-                          console.log('Reading fileNames.txt File data..');
-
-                          let sortedData = JSON.stringify(
-                            JSON.parse(data).split(' ').sort().join(' ')
-                          );
-
-                          fs.writeFile(
-                            'fileNames.txt',
-                            sortedData,
-                            'utf-8',
-                            (error) => {
-                              if (error) {
-                                throw new Error(
-                                  'fileNames.txt file is does not exist..'
-                                );
-                              } else {
-                                console.log('fileNames.txt is created ..');
-
-                                fs.readFile(
-                                  'fileNames.txt',
-                                  'utf-8',
-                                  (error, data) => {
-                                    if (error) {
-                                      throw new Error(
-                                        'fileNames.txt file is does not exist..'
-                                      );
-                                    } else {
-                                      console.log(data);
-
-                                      fs.unlink(
-                                        path.join(__dirname, 'fileNames.txt'),
-                                        (error) => {
-                                          if (error) {
-                                            console.log(error);
-                                          }
-                                        }
-                                      );
-                                    }
-                                  }
-                                );
-                              }
-                            }
-                          );
-                        }
-                      });
-                    }
-                  }
-                );
-              }
-            });
-          }
-        });
+        resolve(`${filename} is created sucessfully..`);
       }
     });
-  } catch (error) {
-    console.log(error.message);
-  }
+  });
 };
 
-module.exports = fileSystemOperation;
+const DeleteFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(filename, (error) => {
+      if (error) {
+        reject('File Not delete..');
+      } else {
+        resolve(`${filename} is Deleted`);
+      }
+    });
+  });
+};
+
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf-8', (error, data) => {
+      if (error) {
+        reject(`Error is ${error}`);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+module.exports = { writeFile, readFile, DeleteFile };
