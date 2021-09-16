36c886e84e77526650b88ae529a8bcd8d790e11c
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 10:34:43 +0530

    import promise from problem2.js file and perform operation by promise chaining.

--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,7 +1,47 @@
-const fileSystemOperation = require('../problem2');
+const { writeFile, readFile, DeleteFile } = require('../problem2');
+const path = require('path');
 
-try {
-  fileSystemOperation();
-} catch (error) {
-  console.log(error.message);
-}
+const fileSystemOperation = () => {
+  readFile('lipsum.txt')
+    .then((data) => {
+      let upperCaseData = data.toUpperCase();
+      return writeFile('fileNames.txt', upperCaseData);
+    })
+    .then((msg) => {
+      console.log(msg);
+      return readFile('fileNames.txt');
+    })
+    .then((data) => {
+      let lowerCaseData = JSON.stringify(
+        data.toLowerCase().split('.').join('')
+      );
+      return writeFile('fileNames.txt', lowerCaseData);
+    })
+    .then((msg) => {
+      console.log(msg);
+      return readFile('fileNames.txt');
+    })
+    .then((data) => {
+      let sortedData = JSON.stringify(
+        JSON.parse(data).split(' ').sort().join(' ')
+      );
+
+      return writeFile('fileNames.txt', sortedData);
+    })
+    .then((msg) => {
+      console.log(msg);
+      return readFile('fileNames.txt');
+    })
+    .then((data) => {
+      console.log(data);
+      return DeleteFile('fileNames.txt');
+    })
+    .then((msg) => {
+      console.log(msg);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+fileSystemOperation();
