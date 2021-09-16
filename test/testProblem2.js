94e4d71de4130e2a65b78c8f53bb83337b48fa7e
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 11:10:36 +0530

    declare fileSystemOperation function as async and perform file operation by async-await keyword.

--- test/testProblem2.js
+++ test/testProblem2.js
@@ -1,47 +1,31 @@
 const { writeFile, readFile, DeleteFile } = require('../problem2');
 const path = require('path');
 
-const fileSystemOperation = () => {
-  readFile('lipsum.txt')
-    .then((data) => {
-      let upperCaseData = data.toUpperCase();
-      return writeFile('fileNames.txt', upperCaseData);
-    })
-    .then((msg) => {
-      console.log(msg);
-      return readFile('fileNames.txt');
-    })
-    .then((data) => {
-      let lowerCaseData = JSON.stringify(
-        data.toLowerCase().split('.').join('')
-      );
-      return writeFile('fileNames.txt', lowerCaseData);
-    })
-    .then((msg) => {
-      console.log(msg);
-      return readFile('fileNames.txt');
-    })
-    .then((data) => {
-      let sortedData = JSON.stringify(
-        JSON.parse(data).split(' ').sort().join(' ')
-      );
+const fileSystemOperation = async () => {
+  let dataOfLipsum = await readFile('lipsum.txt');
+  let upperCaseData = dataOfLipsum.toUpperCase();
 
-      return writeFile('fileNames.txt', sortedData);
-    })
-    .then((msg) => {
-      console.log(msg);
-      return readFile('fileNames.txt');
-    })
-    .then((data) => {
-      console.log(data);
-      return DeleteFile('fileNames.txt');
-    })
-    .then((msg) => {
-      console.log(msg);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+  await writeFile('fileNames.txt', upperCaseData);
+
+  let dataOfWriteFile = await readFile('fileNames.txt');
+  let lowerCaseData = JSON.stringify(
+    dataOfWriteFile.toLowerCase().split('.').join('')
+  );
+
+  await writeFile('fileNames.txt', lowerCaseData);
+
+  let dataOfLowerCase = await readFile('fileNames.txt');
+  let sortedData = JSON.stringify(
+    JSON.parse(dataOfLowerCase).split(' ').sort().join(' ')
+  );
+
+  await writeFile('fileNames.txt', sortedData);
+
+  let finalDataOfFile = await readFile('fileNames.txt');
+
+  console.log(finalDataOfFile);
+
+  await DeleteFile('fileNames.txt');
 };
 
 fileSystemOperation();
