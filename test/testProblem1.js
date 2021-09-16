8b547371ed06476e2be0a5c17a1e1f9fb7610ae0
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-09-16 09:42:22 +0530

    import createDirectory, writeFile, DeleteFile, removeDirecotry promise and perform action inside fileSystemANdRemoving function.

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,10 +1,50 @@
-const fileSystemAndRemoving = require('../problem1');
+const {
+  createDirectory,
+  writeFile,
+  DeleteFile,
+  removeDirectory,
+} = require('../problem1');
 
-let number = Math.random();
-let data = { name: 'dharmendra', age: 24 };
+const fileSystemAndRemoving = () => {
+  let number = Number((Math.random() * 10).toFixed(0));
+  let jsonData = JSON.stringify({ name: 'dharmendra', age: 24 });
+  const dirname = './RandomJsonFile';
 
-try {
-  fileSystemAndRemoving(data, Number((number * 10).toFixed(0)));
-} catch (error) {
-  console.log(error.message);
-}
+  createDirectory(dirname)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error.message));
+
+  for (let index = 1; index <= number; index++) {
+    let filename = `${dirname}/${index}.json`;
+    writeFile(filename, jsonData)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  for (let index = 1; index <= number; index++) {
+    let filename = `${dirname}/${index}.json`;
+    DeleteFile(filename)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  removeDirectory(dirname)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+fileSystemAndRemoving();
